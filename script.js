40ff7a6824d31bb8bbfa47c3cf549b6274800172
vanhog <dieter@hoogestraat.dk>
2025-12-09 12:41:22 +0100

    First beta

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showSomePopup() {
   var thisPopup = document.getElementById('authorline');
   var thisIcon = document.getElementById('shareline');
-  thisPopup.classList.toggle('hidden');
+  //thisPopup.classList.toggle('hidden');
   thisIcon.classList.toggle('hidden');
 }
